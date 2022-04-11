087cbe983bc9d00506fe5b007875c79c0ff5922e
Samap0510 <samael.camargo@correounivalle.edu.co>
2022-04-11 01:23:02 -0500

    Login app

--- app/src/main/java/com/app/ecommerce_univalle/MainActivity.java
+++ app/src/main/java/com/app/ecommerce_univalle/MainActivity.java
@@ -1,8 +1,14 @@
 package com.app.ecommerce_univalle;
 
+import static android.widget.Toast.LENGTH_SHORT;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView username = (TextView) findViewById(R.id.username);
+        TextView password = (TextView) findViewById(R.id.password);
+
+        Button login_btn = (Button) findViewById(R.id.login_btn);
+
+        //admin and admin
+        /*Accion cuando se da click en el botón LOGIN
+         *Si el usuario y la contraseña es admin - admin va a mandar un mensaje diciendo login exitoso
+         *Sino login fallido
+         */
+        login_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (username.getText().toString().equals("admin") &&
+                        password.getText().toString().equals("admin")) {
+                    //Correcto
+                    Toast.makeText(MainActivity.this,"LOGIN SUCCESSFUL", LENGTH_SHORT).show();
+                }else {
+                    //Incorrecto
+                    Toast.makeText(MainActivity.this, "LOGIN FAILED!!!", LENGTH_SHORT).show();
+                }
+            }
+        });
+
     }
 }
